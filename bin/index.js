eb8046c5a9ac4928688a4f010db0c10192ff2196
Alek <alek.salazar@icloud.com>
2021-10-01 17:47:30 -0500

    Addition of show on help

--- bin/index.js
+++ bin/index.js
@@ -4,46 +4,60 @@ const chalk = require("chalk");
 const { TYPES } = require("./constants.json");
 const client = require("./vikings/actions");
 
-yargs.command({
-  command: "backup",
-  describe: "Backs up your world or character based on the type & name flag.",
-  builder: {
-    type: {
-      describe:
-        "Choose your desired type to backup, either 'world, 'viking', or 'both'.",
-      type: "string",
-      demandOption: true,
+const usageStatement = chalk.bold(
+  chalk.cyanBright(
+    `Welcome, i am ${chalk.underline(
+      "Odinserv"
+    )}\n I can safegaurd your home and your viking. Read my commands below.`
+  )
+);
+yargs
+  .usage(usageStatement)
+  .showHelpOnFail(true)
+  .command({
+    command: "backup",
+    describe: `Backs up your world or character based on the type & name flag\nType ${chalk.greenBright(
+      "odin backup --help"
+    )} for a helpful list brOther.`,
+    builder: {
+      type: {
+        describe:
+          "Choose your desired type to backup, either 'world, 'viking', or 'both'.",
+        type: "string",
+        demandOption: true,
+      },
+      name: {
+        describe:
+          "Choose the specific instance to backup, this should be the name of your world, viking, or 'all'. Defaults to all",
+        type: "string",
+        demandOption: false,
+      },
     },
-    name: {
-      describe:
-        "Choose the specific instance to backup, this should be the name of your world, viking, or 'all'. Defaults to all",
-      type: "string",
-      demandOption: false,
+    handler: function (argv) {
+      switch (argv.type) {
+        case TYPES.VIKING:
+          if (typeof argv.name == "string" && argv.name.length > 0) {
+            client.backupViking(argv.name);
+          } else {
+            client.backupAllVikings();
+          }
+          break;
+        case TYPES.WORLD:
+          console.log("World selected!");
+          break;
+        default:
+          console.log(
+            chalk.redBright(
+              `Invalid type: Please try ${chalk.greenBright(
+                "'odin --help'"
+              )} for help`
+            )
+          );
+          break;
+      }
     },
-  },
-  handler: function (argv) {
-    switch (argv.type) {
-      case TYPES.VIKING:
-        if (typeof argv.name == "string" && argv.name.length > 0) {
-          client.backupViking(argv.name);
-        } else {
-          client.backupAllVikings();
-        }
-        break;
-      case TYPES.WORLD:
-        console.log("World selected!");
-        break;
-      default:
-        console.log(
-          chalk.redBright(
-            `Invalid type: Please try ${chalk.greenBright(
-              "'odin --help'"
-            )} for help`
-          )
-        );
-        break;
-    }
-  },
-});
+  })
+  .demandCommand()
+  .help();
 
 yargs.parse();
